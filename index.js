e1bf5e0649cd83211658fd87402779fda200c126
Ian Moffitt <IMoffitt@barkleyrei.com>
2017-06-01 06:41:13 -0400

    More updates; Pokemon module beginnings

--- index.js
+++ index.js
@@ -13,7 +13,7 @@ var requestInMotionFlag = false;
 bot.connect();
 
 bot.onConnect(function() {
-  console.log(' -=- > Connect');
+  // console.log(' -=- > Connect');
 
   var rooms = config.rooms;
   for (var r in rooms) {
@@ -21,19 +21,19 @@ bot.onConnect(function() {
   }
 
   // fetch and print roster contacts (buddy list)
-  this.getRoster(function(err, items, stanza) {
-    if (err) {
-      console.log(' -=- > Error getting roster: ' + err);
-      return;
-    }
-    items.forEach(function(item) {
-      console.log(' -=- > Roster contact: ' + item.name);
-    });
-  });
+  // this.getRoster(function(err, items, stanza) {
+  //   if (err) {
+  //     console.log(' -=- > Error getting roster: ' + err);
+  //     return;
+  //   }
+  //   items.forEach(function(item) {
+  //     console.log(' -=- > Roster contact: ' + item.name);
+  //   });
+  // });
 });
 
 bot.onMessage('catbot', function(channel, from, message) {
-	console.log(' -=- > ' + from + '@' + channel + ' said: ' + message);
+	// console.log(' -=- > ' + from + '@' + channel + ' said: ' + message);
 
 	this.message(channel, '(catbot)');
 });
@@ -50,32 +50,101 @@ bot.onMessage(/^pokemon.+/, function(channel, from, message) {
 		!requestInMotionFlag &&
 		!cooldownFlag) {
 
+		console.log('valid pokemon request');
+
 		requestInMotionFlag = true;
 		var pkmn = msg[1];
 
-		request('http://pokeapi.co/api/v2/pokemon/' + pkmn, function (error, response, body) {
-		    if (!error && response.statusCode == 200) {
-				pokemon = JSON.parse(body);
+		if (pkmn === "help") {
+			self.message(channel, "`pokemon name/number` or `pokemon help` to get this message. Gen 6 or lower. Uses https://pokeapi.co");
 
-				// console.log(pokemon.sprites);
+			cooldown();
+		} else {
 
-				if (pokemon["sprites"].front_default !== null) {
-					self.message(channel, pokemon.sprites.front_default);
+			// IDEA: Configuration of funny responses to specific triggers
+			if (pkmn === "missingno") {
+				self.message(channel, "http://i.imgur.com/YyChnZM.gif");
 
-					requestInMotionFlag = false;
-					cooldownFlag = true;
+				cooldown();
+			} else {
 
-					setTimeout(function () {
-						cooldownFlag = false;
-					}, 3000);
-				}
-		    } else {
-				self.message(channel, '(shrug)');
+				var sprite = '';
+				var name = '';
+				var number = '';
+
+				// Get base info
+				request('http://pokeapi.co/api/v2/pokemon/' + pkmn, function (error, response, body) {
+				    if (!error && response.statusCode == 200) {
+						pokemon = JSON.parse(body);
+
+						console.log('requesting ' + pkmn + ' base info');
+
+						var sprites = pokemon["sprites"];
+						if (sprites.front_default !== null) {
+							sprite = sprites.front_default;
+						}
+
+						number = pokemon["id"];
+						name = pokemon["name"];
+
+						var species_url = pokemon["species"].url;
+
+						// Get name
+						request(species_url, function (error, response, body) {
+						    if (!error && response.statusCode == 200) {
+								species = JSON.parse(body);
+
+								console.log('requesting pokemon species info');
+
+								var names = species["names"];
+
+								for (var n in names) {
+									if (names.hasOwnProperty(n)) {
+
+										var lang = names[n];
+
+										if (lang["language"].name === "en") {
+											name = lang["name"];
+										}
+
+									}
+								}
+
+								self.message(channel, '#' + number + ': ' + name);
+								if (sprite != '') {
+									self.message(channel, sprite);
+								} else {
+									self.message(channel, 'No sprite found =(');
+								}
+
+								cooldown();
+
+							} else {
+
+								self.message(channel, 'Unable to retrieve species info =(');
+								if (sprite != '') {
+									self.message(channel, sprite);
+								} else {
+									self.message(channel, 'No sprite found =(');
+								}
+
+								cooldown();
+
+							}
+						});
+
+
+				    } else {
+
+						self.message(channel, '(shrug)');
+
+						cooldown();
+
+					}
+				});
 			}
-		});
-
+		}
 	}
-
 });
 
 // Kill
@@ -86,3 +155,14 @@ bot.onMessage('!goaway', function(channel, from, message) {
 	}
 
 });
+
+function cooldown() {
+
+	requestInMotionFlag = false;
+	cooldownFlag = true;
+
+	setTimeout(function () {
+		cooldownFlag = false;
+	}, 3000);
+
+}
