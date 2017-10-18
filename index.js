eeaed3d1fd824683da4963eadc701e5baad94d11
Ian Moffitt <IMoffitt@barkleyrei.com>
2017-10-18 14:09:10 -0400

    feat: Abstract pokemon functionality into module

--- index.js
+++ index.js
@@ -1,10 +1,11 @@
 var wobot = require('wobot');
 var config = require('./bot.json');
 var request = require('request');
+const pokemon = require('./modules/pokemon.js');
 
 var bot = new wobot.Bot({
-  jid: config.env.jid,
-  password: config.env.password
+	jid: config.env.jid,
+	password: config.env.password
 });
 
 var cooldownFlag = false;
@@ -12,146 +13,207 @@ var requestInMotionFlag = false;
 
 bot.connect();
 
-bot.onConnect(function() {
-  // console.log(' -=- > Connect');
+bot.onConnect(function () {
+	// console.log(' -=- > Connect');
 
-  var rooms = config.rooms;
-  for (var r in rooms) {
-	  this.join(rooms[r]);
-  }
+	var rooms = config.rooms;
+	for (var r in rooms) {
+		this.join(rooms[r]);
+	}
 
-  // fetch and print roster contacts (buddy list)
-  // this.getRoster(function(err, items, stanza) {
-  //   if (err) {
-  //     console.log(' -=- > Error getting roster: ' + err);
-  //     return;
-  //   }
-  //   items.forEach(function(item) {
-  //     console.log(' -=- > Roster contact: ' + item.name);
-  //   });
-  // });
+	// fetch and print roster contacts (buddy list)
+	// this.getRoster(function(err, items, stanza) {
+	//   if (err) {
+	//     console.log(' -=- > Error getting roster: ' + err);
+	//     return;
+	//   }
+	//   items.forEach(function(item) {
+	//     console.log(' -=- > Roster contact: ' + item.name);
+	//   });
+	// });
 });
 
-bot.onMessage('catbot', function(channel, from, message) {
+bot.onMessage('catbot', function (channel, from, message) {
 	// console.log(' -=- > ' + from + '@' + channel + ' said: ' + message);
 
 	this.message(channel, '(catbot)');
 });
 
-bot.onMessage(/^pokemon.+/, function(channel, from, message) {
+bot.onMessage(/^pokemon.+/, function (channel, from, message) {
 
-	var pokemon = {};
+	var p = {};
 	var self = this;
 
 	var msg = message.split(' ');
 
-	if (msg[0] == "pokemon" &&
+	var alert = '';
+
+	// How I want the API to work...
+	// var response = pokemon.retrieve(msg);
+
+	if (msg[0] == 'pokemon' &&
 		typeof msg[2] === 'undefined' &&
 		!requestInMotionFlag &&
 		!cooldownFlag) {
 
-		console.log('valid pokemon request');
-
 		requestInMotionFlag = true;
-		var pkmn = msg[1];
 
-		if (pkmn === "help") {
-			self.message(channel, "`pokemon name/number` or `pokemon help` to get this message. Gen 6 or lower. Uses https://pokeapi.co");
+		response = pokemon.retrieve(msg[1]).then(function (res) {
+
+			for (var i in res) {
+				if (res.hasOwnProperty(i)) {
+					this.message(channel, res[i]);
+				}
+			}
 
 			cooldown();
-		} else {
 
-			// IDEA: Configuration of funny responses to specific triggers
-			if (pkmn === "missingno") {
-				self.message(channel, "http://i.imgur.com/YyChnZM.gif");
+		}.bind(this));
 
-				cooldown();
-			} else {
-
-				var sprite = '';
-				var name = '';
-				var number = '';
-
-				// Get base info
-				request('http://pokeapi.co/api/v2/pokemon/' + pkmn, function (error, response, body) {
-				    if (!error && response.statusCode == 200) {
-						pokemon = JSON.parse(body);
-
-						console.log('requesting ' + pkmn + ' base info');
-
-						var sprites = pokemon["sprites"];
-						if (sprites.front_default !== null) {
-							sprite = sprites.front_default;
-						}
-
-						number = pokemon["id"];
-						name = pokemon["name"];
-
-						var species_url = pokemon["species"].url;
-
-						// Get name
-						request(species_url, function (error, response, body) {
-						    if (!error && response.statusCode == 200) {
-								species = JSON.parse(body);
-
-								console.log('requesting pokemon species info');
-
-								var names = species["names"];
-
-								for (var n in names) {
-									if (names.hasOwnProperty(n)) {
-
-										var lang = names[n];
-
-										if (lang["language"].name === "en") {
-											name = lang["name"];
-										}
-
-									}
-								}
-
-								self.message(channel, '#' + number + ': ' + name);
-								if (sprite != '') {
-									self.message(channel, sprite);
-								} else {
-									self.message(channel, 'No sprite found =(');
-								}
-
-								cooldown();
-
-							} else {
-
-								self.message(channel, 'Unable to retrieve species info =(');
-								if (sprite != '') {
-									self.message(channel, sprite);
-								} else {
-									self.message(channel, 'No sprite found =(');
-								}
-
-								cooldown();
-
-							}
-						});
-
-
-				    } else {
-
-						self.message(channel, '(shrug)');
-
-						cooldown();
-
-					}
-				});
-			}
-		}
 	}
+
 });
 
+// Old
+//
+// bot.onMessage(/^pokemon.+/, function (channel, from, message) {
+//
+// 	var pokemon = {};
+// 	var self = this;
+//
+// 	var msg = message.split(' ');
+//
+// 	var alert = '';
+//
+// 	// How I want the API to work...
+// 	// var response = pokemon.retrieve(msg);
+//
+// 	if (msg[0] == 'pokemon' &&
+// 		typeof msg[2] === 'undefined' &&
+// 		!requestInMotionFlag &&
+// 		!cooldownFlag) {
+//
+// 		console.log('valid pokemon request');
+//
+// 		requestInMotionFlag = true;
+// 		var pkmn = msg[1];
+//
+// 		if (pkmn === 'help') {
+// 			self.message(channel, '`pokemon name/number` or `pokemon help` to get this message. Gen 6 or lower. Uses https://pokeapi.co');
+//
+// 			cooldown();
+// 		} else {
+//
+// 			if (pkmn.indexOf('!') > -1) {
+// 				alert = ' @here';
+// 				pkmn = pkmn.replace(/!/g, '');
+// 			}
+//
+// 			// IDEA: Configuration of funny responses to specific triggers
+// 			if (pkmn === 'missingno') {
+// 				self.message(channel, 'http://i.imgur.com/YyChnZM.gif');
+//
+// 				cooldown();
+// 			} else {
+//
+// 				if (pkmn === 'pikablu') {
+// 					pkmn = '183';
+// 				}
+//
+// 				pkmn = pkmn.toLowerCase();
+//
+// 				var sprite = '';
+// 				var name = '';
+// 				var number = '';
+//
+// 				// Get base info
+// 				request('http://pokeapi.co/api/v2/pokemon/' + pkmn, function (error, response, body) {
+// 					if (!error && response.statusCode == 200) {
+// 						pokemon = JSON.parse(body);
+//
+// 						console.log('requesting ' + pkmn + ' base info');
+//
+// 						var sprites = pokemon['sprites'];
+// 						if (sprites.front_default !== null) {
+// 							sprite = sprites.front_default;
+// 						}
+//
+// 						number = pokemon['id'];
+// 						name = pokemon['name'];
+//
+// 						var species_url = pokemon['species'].url;
+//
+// 						// Get name
+// 						request(species_url, function (error, response, body) {
+// 							if (!error && response.statusCode == 200) {
+// 								species = JSON.parse(body);
+//
+// 								console.log('requesting pokemon species info');
+//
+// 								var names = species['names'];
+//
+// 								for (var n in names) {
+// 									if (names.hasOwnProperty(n)) {
+//
+// 										var lang = names[n];
+//
+// 										if (lang['language'].name === 'en') {
+// 											name = lang['name'];
+// 										}
+//
+// 									}
+// 								}
+//
+// 								self.message(channel, '#' + number + ': ' + name + alert);
+// 								if (sprite != '') {
+// 									self.message(channel, sprite);
+// 								} else {
+// 									self.message(channel, 'No sprite found =(');
+// 								}
+//
+// 								cooldown();
+//
+// 							} else {
+//
+// 								self.message(channel, 'Unable to retrieve species info =(');
+// 								if (sprite != '') {
+// 									self.message(channel, sprite);
+// 								} else {
+// 									self.message(channel, 'No sprite found =(');
+// 								}
+//
+// 								cooldown();
+//
+// 							}
+// 						});
+//
+// 					} else {
+//
+// 						self.message(channel, '(shrug)');
+//
+// 						cooldown();
+//
+// 					}
+// 				});
+// 			}
+// 		}
+// 	}
+// });
+
 // Kill
-bot.onMessage('!goaway', function(channel, from, message) {
+bot.onMessage('!goaway', function (channel, from, message) {
 
 	if (from == config.boss) {
 		this.disconnect();
+	}
+
+});
+
+bot.onMessage('!die', function (channel, from, message) {
+
+	if (from == config.boss) {
+		this.disconnect();
+		process.exit();
 	}
 
 });
@@ -163,6 +225,6 @@ function cooldown() {
 
 	setTimeout(function () {
 		cooldownFlag = false;
-	}, 3000);
+	}, 2000);
 
 }
---
+++ modules/pokemon.js
@@ -0,0 +1,224 @@
+const util = require('util');
+const request = require('request');
+
+module.exports = {
+
+	special: [
+		{
+			'key': 'missingno',
+			'response': 'manual',
+			'manual': 'http://i.imgur.com/YyChnZM.gif'
+		},
+		{
+			'key': 'pikablu',
+			'response': 'api',
+			'api': '183'
+		}
+	],
+
+	endpoint: 'http://pokeapi.co/api/v2/pokemon/%s',
+
+	retrieve: function (msg) {
+
+		console.log('pokemon - inside retrieve');
+		console.log('msg - ', msg);
+
+		var response = [];
+		var alert = 0;
+
+		if (msg.indexOf('!') > -1) {
+			alert = 1;
+			msg = msg.replace(/!/g, '');
+		}
+
+		// Check for help
+		if (msg === 'help') {
+			response.push('`pokemon name/number` or `pokemon help` to get this message. Gen 6 or lower. Uses https://pokeapi.co');
+		}
+
+		// Check special
+		for (var j in this.special) {
+			if (msg === this.special[j].key) {
+				return this.parseSpecial(this.special[j]);
+			}
+		}
+
+		// Check API or return existing response
+		if (response.length > 0) {
+			return new Promise(function (resolve, reject) {
+				resolve(response);
+			});
+		} else {
+			return this.api(msg);
+		}
+
+	},
+
+	parseSpecial: function (special) {
+
+		switch (special.response) {
+
+			case 'manual':
+
+				return Promise.resolve([special.manual]);
+
+				break;
+
+			case 'api':
+
+				return new Promise(function (resolve, reject) {
+					this.api(special.api).then(function (res) {
+						resolve(res);
+					});
+				}.bind(this));
+
+				break;
+
+			default:
+
+				return new Promise.resolve(['(shrug)']);
+
+				break;
+
+		}
+
+	},
+
+	api: function (key, alert) {
+
+		var response = [];
+
+		return new Promise(function (resolve, reject) {
+
+			this._apiGetPokemon(key).then(function (pokemon) {
+
+				this._apiGetInfo(pokemon).then(function (pokemon) {
+
+					response.push(
+						util.format('#%s: %s%s',
+							pokemon['id'],
+							pokemon['name'],
+							(alert) ? ' @here' : ''
+						)
+					);
+
+					response.push(pokemon['sprite']);
+
+					resolve(response);
+
+				}.bind(this), function (error) {
+
+					response.push(error);
+
+					return reject(response);
+
+				}.bind(this));
+
+			}.bind(this), function (error) {
+
+				response.push(error);
+
+				return reject(response);
+
+			}.bind(this));
+
+		}.bind(this));
+
+	}
+	,
+
+	_apiGetPokemon: function (key) {
+
+		var pokemon = {};
+		var end = util.format(this.endpoint, key);
+
+		return new Promise(function (resolve, reject) {
+
+			request(end, function (error, response, body) {
+
+				console.log('requesting initial endpoint');
+
+				if (error) {
+					return reject(new Error(error));
+				}
+
+				if (response.statusCode !== 200) {
+					return reject(new Error('bad status code'));
+				}
+
+				pokemon = JSON.parse(body);
+
+				resolve(pokemon);
+
+			});
+
+		});
+
+	}
+	,
+
+	_apiGetInfo: function (pokemon) {
+
+		var species = {};
+		var name = '';
+		var sprite = '';
+
+		var url = pokemon['species'].url;
+
+		var sprites = pokemon['sprites'];
+		if (sprites.front_default !== null) {
+			sprite = sprites.front_default;
+		} else {
+			sprite = 'No sprite found =(';
+		}
+
+		return new Promise(function (resolve, reject) {
+
+			request(url, function (error, response, body) {
+
+				console.log('requesting pokemon info');
+
+				var res = {};
+
+				if (error) {
+					return reject(new Error(error));
+				}
+
+				if (response.statusCode !== 200) {
+					return reject(new Error('bad status code'));
+				}
+
+				species = JSON.parse(body);
+
+				var names = species['names'];
+
+				for (var n in names) {
+					if (names.hasOwnProperty(n)) {
+
+						var lang = names[n];
+
+						if (lang['language'].name === 'en') {
+							name = lang['name'];
+						}
+
+					}
+				}
+
+				res = {
+					id: pokemon['id'],
+					name: name,
+					sprite: sprite
+				};
+
+				console.log(res);
+
+				resolve(res);
+
+			});
+
+		});
+
+	}
+
+}
+;
