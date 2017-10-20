fc9ed5aef122d48b36224b959f85eed642bd5f91
Ian Moffitt <IMoffitt@barkleyrei.com>
2017-10-20 08:55:52 -0400

    feat: Add markov chain functionality

--- index.js
+++ index.js
@@ -1,7 +1,10 @@
-var wobot = require('wobot');
-var config = require('./bot.json');
-var request = require('request');
+const wobot = require('wobot');
+const config = require('./bot.json');
 const pokemon = require('./modules/pokemon.js');
+const fs = require('fs');
+const megahal = require('jsmegahal');
+
+var hal = new megahal(4);
 
 var bot = new wobot.Bot({
 	jid: config.env.jid,
@@ -11,32 +14,30 @@ var bot = new wobot.Bot({
 var cooldownFlag = false;
 var requestInMotionFlag = false;
 
+var markov = __dirname + '/markov.txt';
+
 bot.connect();
 
 bot.onConnect(function () {
-	// console.log(' -=- > Connect');
-
 	var rooms = config.rooms;
 	for (var r in rooms) {
 		this.join(rooms[r]);
+		// this.message(rooms[r], '(catbot)');
 	}
 
-	// fetch and print roster contacts (buddy list)
-	// this.getRoster(function(err, items, stanza) {
-	//   if (err) {
-	//     console.log(' -=- > Error getting roster: ' + err);
-	//     return;
-	//   }
-	//   items.forEach(function(item) {
-	//     console.log(' -=- > Roster contact: ' + item.name);
-	//   });
-	// });
+	fs.readFile(markov, 'utf8', function(err, data) {
+		hal.addMass(data);
+	});
 });
 
 bot.onMessage('catbot', function (channel, from, message) {
 	// console.log(' -=- > ' + from + '@' + channel + ' said: ' + message);
 
-	this.message(channel, '(catbot)');
+	// this.message(channel, '(catbot)');
+
+	var messageSansName = message.replace(/catbot/i, '');
+
+	this.message(channel, hal.getReplyFromSentence(messageSansName));
 });
 
 bot.onMessage(/^pokemon.+/, function (channel, from, message) {
@@ -68,152 +69,58 @@ bot.onMessage(/^pokemon.+/, function (channel, from, message) {
 
 			cooldown();
 
+		}.bind(this), function (error) {
+			console.log(error);
+			this.message(channel, '(shrug) [whoopsie: ' + error + ']');
 		}.bind(this));
 
 	}
 
 });
 
-// Old
-//
-// bot.onMessage(/^pokemon.+/, function (channel, from, message) {
-//
-// 	var pokemon = {};
-// 	var self = this;
-//
-// 	var msg = message.split(' ');
-//
-// 	var alert = '';
-//
-// 	// How I want the API to work...
-// 	// var response = pokemon.retrieve(msg);
-//
-// 	if (msg[0] == 'pokemon' &&
-// 		typeof msg[2] === 'undefined' &&
-// 		!requestInMotionFlag &&
-// 		!cooldownFlag) {
-//
-// 		console.log('valid pokemon request');
-//
-// 		requestInMotionFlag = true;
-// 		var pkmn = msg[1];
-//
-// 		if (pkmn === 'help') {
-// 			self.message(channel, '`pokemon name/number` or `pokemon help` to get this message. Gen 6 or lower. Uses https://pokeapi.co');
-//
-// 			cooldown();
-// 		} else {
-//
-// 			if (pkmn.indexOf('!') > -1) {
-// 				alert = ' @here';
-// 				pkmn = pkmn.replace(/!/g, '');
-// 			}
-//
-// 			// IDEA: Configuration of funny responses to specific triggers
-// 			if (pkmn === 'missingno') {
-// 				self.message(channel, 'http://i.imgur.com/YyChnZM.gif');
-//
-// 				cooldown();
-// 			} else {
-//
-// 				if (pkmn === 'pikablu') {
-// 					pkmn = '183';
-// 				}
-//
-// 				pkmn = pkmn.toLowerCase();
-//
-// 				var sprite = '';
-// 				var name = '';
-// 				var number = '';
-//
-// 				// Get base info
-// 				request('http://pokeapi.co/api/v2/pokemon/' + pkmn, function (error, response, body) {
-// 					if (!error && response.statusCode == 200) {
-// 						pokemon = JSON.parse(body);
-//
-// 						console.log('requesting ' + pkmn + ' base info');
-//
-// 						var sprites = pokemon['sprites'];
-// 						if (sprites.front_default !== null) {
-// 							sprite = sprites.front_default;
-// 						}
-//
-// 						number = pokemon['id'];
-// 						name = pokemon['name'];
-//
-// 						var species_url = pokemon['species'].url;
-//
-// 						// Get name
-// 						request(species_url, function (error, response, body) {
-// 							if (!error && response.statusCode == 200) {
-// 								species = JSON.parse(body);
-//
-// 								console.log('requesting pokemon species info');
-//
-// 								var names = species['names'];
-//
-// 								for (var n in names) {
-// 									if (names.hasOwnProperty(n)) {
-//
-// 										var lang = names[n];
-//
-// 										if (lang['language'].name === 'en') {
-// 											name = lang['name'];
-// 										}
-//
-// 									}
-// 								}
-//
-// 								self.message(channel, '#' + number + ': ' + name + alert);
-// 								if (sprite != '') {
-// 									self.message(channel, sprite);
-// 								} else {
-// 									self.message(channel, 'No sprite found =(');
-// 								}
-//
-// 								cooldown();
-//
-// 							} else {
-//
-// 								self.message(channel, 'Unable to retrieve species info =(');
-// 								if (sprite != '') {
-// 									self.message(channel, sprite);
-// 								} else {
-// 									self.message(channel, 'No sprite found =(');
-// 								}
-//
-// 								cooldown();
-//
-// 							}
-// 						});
-//
-// 					} else {
-//
-// 						self.message(channel, '(shrug)');
-//
-// 						cooldown();
-//
-// 					}
-// 				});
-// 			}
-// 		}
-// 	}
-// });
-
-// Kill
-bot.onMessage('!goaway', function (channel, from, message) {
-
-	if (from == config.boss) {
-		this.disconnect();
-	}
-
-});
-
-bot.onMessage('!die', function (channel, from, message) {
+bot.onMessage(/!die|!goaway|!reset/, function (channel, from, message) {
 
 	if (from == config.boss) {
 		this.disconnect();
 		process.exit();
+	}
+
+});
+
+bot.onMessage(/(?!.*?catbot)^.*$/ig, function (channel, from, message) {
+
+	var msg = message.split(' ');
+
+	if (msg[0] !== 'pokemon') {
+
+		writeToLog(__dirname + '/markov.txt', message);
+
+		hal.addMass(message);
+
+	}
+
+});
+
+bot.onMessage(/catbot/i, function (channel, from, message) {
+
+	if (!requestInMotionFlag && !cooldownFlag) {
+
+		requestInMotionFlag = true;
+
+		var messageSansName = message.replace(/catbot/i, '');
+
+		writeToLog(__dirname + '/markov.txt', messageSansName);
+
+		setTimeout(function () {
+
+			this.message(channel, hal.getReplyFromSentence(messageSansName));
+
+		}.bind(this), 1500);
+
+		hal.addMass(messageSansName);
+
+		cooldown();
+
 	}
 
 });
@@ -228,3 +135,13 @@ function cooldown() {
 	}, 2000);
 
 }
+
+function writeToLog(file, text) {
+
+	fs.appendFile(file, text + '\r\n', function (err) {
+		if (err) return console.log(err);
+		console.log('log is updated');
+	});
+
+}
+
